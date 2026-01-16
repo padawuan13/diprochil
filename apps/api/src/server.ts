ccaada7510a33f5de961618d46cef6017d9697c3
padawuan13 <felipe.hernandez.p@alumno.iplacex.cl>
2026-01-16 10:32:45 -0300

    nuevos cambios al server

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -21,16 +21,13 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
-const publicDir = path.resolve(__dirname, "../public"); 
-if (fs.existsSync(publicDir)) {
+const publicDir = path.join(__dirname, "../public"); 
+
   app.use(express.static(publicDir));
 
   app.get("/", (_req, res) => {
     res.sendFile(path.join(publicDir, "index.html"));
   });
-} else {
-  console.warn("⚠️ No se encontró publicDir:", publicDir);
-}
 
 app.use(routes);
 app.use("/exports", exportsRoutes);
@@ -41,5 +38,5 @@ app.use(errorMiddleware);
 
 const port = Number(process.env.PORT ?? 3000);
 app.listen(port, () => {
-  console.log(`✅ API + Web running on port ${port}`);
+  console.log(` API + Web running on port ${port}`);
 });
